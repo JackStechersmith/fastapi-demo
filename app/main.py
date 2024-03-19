03c6fef06341ea23b289bd3eb058d8d0015a5ddb
Jack Stechersmith <jackstechersmith@gmail.com>
2024-03-19 20:12:10 +0000

    saved main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-# import boto3
+import json
+import requests
+import boto3
 
 app = FastAPI()
 
@@ -14,8 +16,17 @@ app = FastAPI()
 # The zone apex is the 'default' page for a URL
 # This will return a simple hello world via GET method.
 @app.get("/")  # zone apex
-def read_root():
-    return {"Hello": "World"}
+def zone_apex():
+    return {"Hello": "World Wide Web"}
+
+@app.get("/github/repos/{user}")
+def github_user_repose(user):
+    url = "https://api.github.com/users/" + user + "/repos"
+    response = requests.get(url)
+    body = json.loads(response.text)
+    return {"repos": body}
+
+
 
 
 # Endpoints and Methods
@@ -30,7 +41,10 @@ def add_me(number_1: int, number_2: int):
     return {"sum": sum}
 
 # Let's develop a new one:
-
+@app.get("/divide/{number_1}/{number_2}")
+def divide_me(number_1: int, number_2: int):
+    division = number_1 / number_2
+    return {"division": division}
 
 ## Parameters
 # Introduce parameter data types and defaults from the Optional library
@@ -85,9 +99,9 @@ def patch_item(item_id: int, item: Item):
 
 
 # Incorporate with boto3: simpler than the `requests` library:
-# @app.get("/aws/s3")
-# def fetch_buckets():
-#     s3 = boto3.client("s3")
-#     response = s3.list_buckets()
-#     buckets = response['Buckets']
-#     return {"buckets": buckets}
+@app.get("/aws/s3")
+def fetch_buckets():
+     s3 = boto3.client("s3")
+     response = s3.list_buckets()
+     buckets = response['Buckets']
+     return {"buckets": buckets}
